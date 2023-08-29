c8b135146dd1bc56c87899e9a430ac2401a8c979
Haru <frankcito639@gmail.com>
2023-08-29 09:04:07 -0500

    agregando Data

--- 02_variables.dart
+++ 02_variables.dart
@@ -9,12 +9,6 @@ void main() {
   
   // dynamic
   
-  dynamic errorMessage = 'Hola';
-  errorMessage = true;
-  errorMessage = [1,2,3,4,5,6];
-  errorMessage = {1,2,3,4,5,6};
-  errorMessage =  () => true;
-  errorMessage = null;
   
   print("""
   $pokemon
--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -1,18 +1,22 @@
 void main() {
   
-  final mySquare = Square(side: -10);
+  final mySquare = Square( side: -10 );
   
-  // mySquare.side = 5;
+//   mySquare.side = 5;
   
-  print( 'Area: ${ mySquare.area }' );
+  
+  print( 'área: ${ mySquare.area }' );
+  
 }
 
 class Square {
+  
   double _side; // side * side
   
-  Square({ required double side})
-    : assert(side >= 0, 'side must be >= 0'),
-  		_side = side;
+  
+  Square({ required double side })
+     : assert(side >= 0, 'side must be >= 0'),
+       _side = side;
   
   double get area {
     return _side * _side;
@@ -20,11 +24,15 @@ class Square {
   
   set side( double value ) {
     print('setting new value $value');
-    if ( value < 0) throw 'Value must be >= 0';
+    if ( value < 0 ) throw 'Value must be >=0';
+    
     _side = value;
+      
   }
+  
   
   double calculateArea() {
     return _side * _side;
   }
 }
+
