cd0ffa7fc8fa7efba69e974cf6b9c110f236d48f
Haru <frankcito639@gmail.com>
2023-08-29 09:04:16 -0500

    Agregando data

--- 01_hello_world.dart
+++ 01_hello_world.dart
@@ -1,5 +1,12 @@
 void main() {
-  print(sayHelloWorld());
+  
+//   var myName = 'Fernando';
+//   late final myName;
+//   String myName = 'Fernando';
+  const myName = 'Fernando';
+    
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
 }
-
-String sayHelloWorld() => 'Hello World!';
